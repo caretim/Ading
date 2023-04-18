f4334ea5dba1827cf8e55a89c5adc131cc431e03
caretim <karesin72041@gmail.com>
2023-04-18 22:14:01 +0900

    230418

--- account/models.py
+++ account/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+    followings=models.ManyToManyField('self',symmetrical=False, related_name = 'followers')
+    # 팔로우 설정  유저모델에 종속되도록 
+    @property # 
+    def full_name(self):
+        return f'{self.last_name}{self.first_name}'
+
+
 
 # Create your models here.
--- articles/views.py
+++ articles/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
 from .models import Article
+from .forms import ArticleForm
 # Create your views here.
 
 
@@ -11,3 +12,19 @@ def index(request):
         'articles':articles
     }
     return render (request,'articles/index.html',context)
+
+
+def create(request):
+    if request.method == 'POST':
+        article_form = ArticleForm(request.POST)
+        if article_form.is_valid(): #유효성검사
+            article = article_form.save(commit=True)# 유효하다면 데이터베이스 커밋,
+            article.save()
+            return redirect('articles:index') # 글 작성후 바로 인덱스페이지로 이동하기,
+    else: # 메소드가 post가 아닐시, 빈 폼을 줘서 작성양식을 보여준다,
+        article_form = ArticleForm()
+        context = {
+            'article_form':article_form
+        }
+        return render(request,'articles:form.html', context=context)
+    
