d9fc7a7bbc5e819c6932092aee545ab41efe652b
caretim <karesin72041@gmail.com>
2023-04-20 22:32:12 +0900

    230420

--- articles/views.py
+++ articles/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect, get_object_or_404
 
 from .models import Article
 from .forms import ArticleForm
+from django.contrib import messages
 # Create your views here.
 
 
@@ -36,3 +37,32 @@ def detail(request, pk):
         'article': article,
     }
     return render(request, 'articles/detail.html', context)
+
+
+def update(request, pk):
+    article = get_object_or_404(Article, pk=pk)
+    if request.user == article.user: 
+        if request.method == 'POST':
+            # POST : input 값 가져와서, 검증하고, DB에 저장
+            article_form = ArticleForm(request.POST, request.FILES, instance=article)
+            if article_form.is_valid():
+                # 유효성 검사 통과하면 저장하고, 상세보기 페이지로
+                article_form.save()
+                messages.success(request, '글이 수정되었습니다.')
+                return redirect('articles:detail', article.pk)
+            # 유효성 검사 통과하지 않으면 => context 부터해서 오류메시지 담긴 article_form을 랜더링
+        else:
+            # GET : Form을 제공
+            article_form = ArticleForm(instance=article)
+        context = {
+            'article_form': article_form
+        }
+        return render(request, 'articles/form.html', context)
+    else:
+        # 작성자가 아닐 때
+        # (1) 403 에러메시지를 던져버린다. 
+        # from django.http import HttpResponseForbidden
+        # return HttpResponseForbidden()
+        # (2) flash message 활용!
+        messages.warning(request, '작성자만 수정할 수 있습니다.')
+        return redirect('articles:detail', article.pk)
