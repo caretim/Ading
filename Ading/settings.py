456d02e4bd8388da1c2e15ebb11e1c51dfec6026
caretim <karesin72041@gmail.com>
2023-07-20 23:05:55 +0900

    reboot

--- Ading/settings.py
+++ Ading/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'articles',
-    'accounts',
+    # 'accounts',
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
--- articles/urls.py
+++ articles/urls.py
@@ -1,12 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-from . import views
 
-app_name = 'articles'
+
+
 urlpatterns = [
-   path('',views.index,name='index'),
-   path('create/', views.create, name='create'),
-   path('<int:pk>/', views.detail, name='detail'),
-   path('<int:pk>/update', views.update, name='update'),
-   path('<int:pk>/delete', views.delete, name='delete'),
+    path('', include('articles/main')),
+    # path('accounts/', include('accounts.urls')),
 ]
