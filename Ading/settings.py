8007587ede8b87b957511449a153558f4dbc67af
caretim <karesin72041@gmail.com>
2023-07-26 14:20:53 +0900

    re_base

--- Ading/settings.py
+++ Ading/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = "Ading.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [BASE_DIR/'base'],
+        "DIRS": [BASE_DIR/'templates'],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
--- accounts/views.py
+++ accounts/views.py
@@ -16,3 +16,17 @@ def signup(request):
             'signup_form' : form
         }
     return render(request,'accounts/signup.html',context)
+
+
+def login(request):
+    if request.method == 'POST':
+        form = UserSignup(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('articlse:main')
+    else:
+        form = UserSignup()
+        context ={
+            'signup_form' : form
+        }
+    return render(request,'accounts/signup.html',context)
