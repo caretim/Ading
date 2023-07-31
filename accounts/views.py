320f12a7a134df3b09e6b89b0c56fa31b64926ae
caretim <karesin72041@gmail.com>
2023-07-31 20:25:26 +0900

    accounts_detail

--- accounts/views.py
+++ accounts/views.py
@@ -3,6 +3,9 @@ from .forms import UserSignup
 from django.contrib.messages import get_messages
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import login as my_login ,logout as my_logout 
+from django.contrib.auth.decorators import login_required
+from .models import NewUser
+
 # Create your views here.
 
 
@@ -40,3 +43,15 @@ def logout(request):
     my_logout(request)
 
     return redirect('articles:main')
+
+@login_required
+def detail(request):
+    user_info = NewUser.objects.get(pk=request.user.pk)
+    user_article = user_info.article_set.all()
+    #참조 폴인키로 연결되어있는 오브젝트 반환
+    print(user_article)
+    context ={
+        'user':user_info,
+        'user_article':user_article
+    }
+    return render(request,'accounts/user_detail.html',context)
